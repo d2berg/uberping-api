5ad7add986d560f2b17314fed3f6212f8d29e4c4
Håkon N Berg <hakon.n.berg@gmail.com>
2018-11-18 19:10:27 +0100

    init

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = 3000 || process.env.PORT;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`api listening on port ${port}!`))
